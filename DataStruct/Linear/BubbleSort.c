efb0b8e0ad3a60203978688d8034455754ff2ca5
Zara <zacharius02dysart@gmail.com>
2026-09-25 18:24:32 -0700

    Fixed the sorting and swapping func

--- DataStruct/Linear/BubbleSort.c
+++ DataStruct/Linear/BubbleSort.c
@@ -6,25 +6,25 @@ int* SortingFunc(int* rdmArr, size_t arraySize);
 
 int main(){
     //Worst Case
-    int rdmArr_Worst[10] = {3,2,4,7,6,9,1,5,8,10};
+    int rdmArr_Worst[10] = {3,2,4,7,6,9,1,5,8,0};
     size_t worstSize = sizeof(rdmArr_Worst)/sizeof(int);
 
 
     //Average Case
-    int rdmArr_Average[10] = {1,2,3,5,4,6,8,7,9,10};
+    int rdmArr_Average[10] = {0,1,2,3,5,4,6,8,7,9};
     size_t averageSize = sizeof(rdmArr_Average)/sizeof(int);
     int* ptrAverage = rdmArr_Average;
     
 
 
     //Best Case
-    int rdmArr_Best[10] = {1,2,3,4,5,8,7,9,6,10};
+    int rdmArr_Best[10] = {1,2,3,4,5,8,7,9,6,0};
     size_t bestSize = sizeof(rdmArr_Best)/sizeof(int);
     int* ptrBest = rdmArr_Best;
     //Computation call
     int* returnResults = SortingFunc(ptrBest, bestSize);
 
-    for(int i = 0; i<=averageSize; i++){
+    for(int i = 0; i<averageSize; i++){
         printf("%d", returnResults[i]);
     }
 
@@ -34,12 +34,13 @@ int main(){
 
 //function
 int* SortingFunc(int* rdmArr, size_t arraySize){
-    for(int i = 0; i<=arraySize; i++){
-        for(int k =1; k<arraySize; k++){
-            if(rdmArr[i]>rdmArr[k]){
-                rdmArr[i] = rdmArr[k];
+    for(int i = 0; i<arraySize; i++){
+        for(int j=i+1; j<arraySize; j++){
+            if(rdmArr[i]>rdmArr[j]){
+                int placeHolder = rdmArr[j];
+                rdmArr[j] = rdmArr[i]; 
+                rdmArr[i] = placeHolder;
             }
-            continue;
         }
     }
     return rdmArr;
